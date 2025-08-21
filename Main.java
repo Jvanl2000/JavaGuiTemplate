55693f3aae79a829a4fed4d7a0c7a3b816e0781b
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2025-08-21 12:55:49 +0000

    2.5

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 
-public class test {
+public class Main {
     public static void main(String[] args) {
+        System.out.println("You Java program has compiled and is now running!");
+
         //creating instance of JFrame
         JFrame f = new JFrame();
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JButton b1 = new JButton("Hello, World!");
         b1.setBounds(90, 100, 180, 40);
