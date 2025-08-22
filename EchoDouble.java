a95de8cd796e098a48e9e19559246b0c2d1f8b0c
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2025-08-22 12:03:01 -0400

    Delete EchoDouble.java

--- EchoDouble.java
+++
@@ -1,8 +0,0 @@
-public class EchoDouble {
-    public static void main(String[] args) {
-        while (!StdIn.isEmpty()) {
-            double value = StdIn.readDouble();
-            StdOut.println(value);
-        }
-    }
-}
