d882b13436639e1927017c5fc18b859c2b6da970
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2025-08-21 21:45:33 -0400

    Add Test class to read and print double values

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        while (!StdIn.isEmpty()) {
+            double value = StdIn.readDouble();
+            StdOut.println(value);
+        }
+    }
+}
