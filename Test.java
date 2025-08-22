f311adee0ac7ae9f1d393e131dfb49151851d9d1
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2025-08-22 02:45:58 +0000

    10

--- Test.java
+++ Test.java
@@ -1,7 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        Complex d = 0;
-        System.out.println(d);
         while (!StdIn.isEmpty()) {
             double value = StdIn.readDouble();
             StdOut.println(value);
